da435e532491fd99996cb620966b5458ff4414cf
Marco Laranja <marco_laranja@hotmail.com>
2017-12-18 16:28:11 -0200

    layout ready

---
+++ js/script.js
@@ -0,0 +1,84 @@
+var play = 0;
+
+  function choice1() {
+    if (play == 0) {
+      document.getElementById('choice1').innerHTML = "X";
+      play = 1;
+    }
+    else {
+      document.getElementById("choice1").innerHTML = "O";
+      play = 0;
+    }
+}
+
+  function choice2() {
+    if (play == 0) {
+      document.getElementById("choice2").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice2").innerHTML = "O";
+    }
+}
+
+  function choice3() {
+    if (play == 0) {
+      document.getElementById("choice3").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice3").innerHTML = "O";
+    }
+}
+
+  function choice4() {
+    if (play == 0) {
+      document.getElementById("choice4").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice4").innerHTML = "O";
+    }
+}
+
+  function choice5() {
+    if (play == 0) {
+      document.getElementById("choice5").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice5").innerHTML = "O";
+    }
+}
+
+  function choice6() {
+    if (play == 0) {
+      document.getElementById("choice6").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice6").innerHTML = "O";
+    }
+}
+
+  function choice7() {
+    if (play == 0) {
+      document.getElementById("choice7").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice7").innerHTML = "O";
+    }
+}
+
+  function choice8() {
+    if (play == 0) {
+      document.getElementById("choice8").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice8").innerHTML = "O";
+    }
+}
+
+  function choice9() {
+    if (play == 0) {
+      document.getElementById("choice9").innerHTML = "X";
+    }
+    else {
+      document.getElementById("choice9").innerHTML = "O";
+    }
+}
